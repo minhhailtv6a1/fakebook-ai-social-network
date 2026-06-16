a0e6ea23b998e824a51160f472165154ca6f576b
Hai Tran <minhhailtv6a1@gmail.com>
2026-06-16 12:28:23 +0700

    Remove backend and frontend from pipeline-training

--- backend/app/ml_dl/label_mapping.py
+++
@@ -1,6 +0,0 @@
-LABEL_MAPPING = {
-    0: "clean",
-    1: "offensive",
-    2: "hate",
-    3: "scam"
-}
--- frontend/next.config.ts
+++
@@ -1,8 +0,0 @@
-import { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // ...giữ nguyên các cấu hình khác của bạn...
-  allowedDevOrigins: ["http://192.168.1.9:3000"], // thêm origin bạn đang dùng
-};
-
-export default nextConfig;
